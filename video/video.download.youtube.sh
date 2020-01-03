4ec68b17e87786106c24ed8aec12cb621b9e7357
Lucian Sirbu <quamis@gmail.com>
2020-01-03 13:52:21 +0200

    added another youtube download helper

---
+++ video/video.download.youtube.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# @requires
+# imagemagik/convert
+
+# usage:
+# 	video.download.youtube.sh URL='https://www.youtube.com/watch?v=T5OkKPrcvT0'
+
+
+# THIS ALLOWS INJECTING VARS into the local namespace
+# might not be very secure, be careful how you declare & check variables
+for ARGUMENT in "$@"; do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2- -d=)   
+	declare $KEY="$VALUE"
+done
+
+: ${LIST:=""};
+: ${URL:=""};
+
+: ${DIR:="./"};
+: ${SPEED_LIMIT_KB:="512"};
+: ${SLEEP_BETWEEN_REQUESTS:="2"};
+
+: ${TMPDIR:="/tmp/"};
+: ${COMMANDFILE:=`mktemp --tmpdir="${TMPDIR}"`};
+: ${VERBOSE:="0"};	# 0, 1
+
+: ${RUN_MODE:="sequential"};	# 'dry-run', 'parallel', 'sequential'
+: ${THREADS:="`parallel --no-notice --number-of-cores`"};
+: ${MAX_LOAD:="95%"};
+
+
+# clear the command list
+echo > "$COMMANDFILE";
+
+if [[ "$LIST" == "" && "$URL" == "" ]]; then
+   echo "Please specify either LIST=filename or URL=http://...";
+   exit;
+fi
+
+# build a list of commands to run
+mkdir -p "${DIR}";
+DIR=`realpath "${DIR}"`;
+
+SAVEIFS=$IFS
+IFS=$'\n'
+if [[ "$LIST" == "" && "$URL" != "" ]]; then
+	LIST="$URL"
+fi;
+
+for URL in `echo "$LIST" | sort`; do
+	# TMP_FILE=`mktemp "${TMPDIR}pdf.XXXXXXXXXXXXXXXXXXXXXXX.m4a"`
+	
+	# DEBUGGING
+	if [ "$VERBOSE" = "1" ]; then
+		echo "URL: $URL"
+	fi;
+
+    # for more options, see the manual
+	CMD="(mkdir -p \"${DIR}\" && youtube-dl --rate-limit ${SPEED_LIMIT_KB}k -f 'bestvideo[height<=720]+bestaudio/bestvideo+bestaudio' --merge-output-format mp4 --write-description --encoding utf-8 \"${URL}\";)"
+	
+	if [ "$VERBOSE" = "1" ]; then
+		echo "$CMD";
+	fi;
+
+	# append to the command list
+	echo "$CMD" >> "$COMMANDFILE";
+done;
+IFS=$SAVEIFS
+
+
+# https://www.msi.umn.edu/support/faq/how-can-i-use-gnu-parallel-run-lot-commands-parallel
+
+# process the command list
+
+# simply display the "to-run" commands
+if [ "$RUN_MODE" = "parallel" ]; then
+	# run the list in paralel
+	# @see https://www.gnu.org/software/parallel/man.html
+	parallel --no-notice --bar --jobs $THREADS --load $MAX_LOAD < "$COMMANDFILE"
+elif [ "$RUN_MODE" = "sequential" ]; then
+	bash -x "$COMMANDFILE";
+elif [ "$RUN_MODE" = "dry-run" ]; then
+	cat "$COMMANDFILE";
+else
+	# TODO: sequential conversion
+	echo "Unknown run mode";
+fi;
+
+rm $COMMANDFILE;
