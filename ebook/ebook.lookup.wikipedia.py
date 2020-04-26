91e5dfd197456892dbbc145f0400993adb59795b
Lucian Sirbu <quamis@gmail.com>
2020-04-26 11:39:19 +0300

    the wiki lookup mostly works

--- ebook/ebook.lookup.wikipedia.py
+++ ebook/ebook.lookup.wikipedia.py
@@ -9,7 +9,7 @@
     MIT License
 """
 
-import re
+import re, argparse
 import requests
 
 # pip3 install -U requests --user
@@ -20,11 +20,15 @@ import requests
 def remove_html_tags(html):
     return re.sub('<.*?>', '', html)
 
+parser = argparse.ArgumentParser(description='TODO')
+parser.add_argument('--text',  '-text',  dest='text',   action='store', type=str, default=None,  help='TODO')
+args = vars(parser.parse_args())
+
 session = requests.Session()
 
 URL = "https://en.wikipedia.org/w/api.php"
 
-SEARCHPAGE = "A. E. Van Vogt"
+SEARCHPAGE = args['text']
 
 PARAMS = {
     "action": "query",
@@ -39,20 +43,43 @@ PARAMS = {
 request = session.get(url=URL, params=PARAMS)
 response = request.json()
 
-print(response)
-exit()
-
-
 authorsList = []
 for match in response['query']['search']:
     textSnippet = remove_html_tags(match['snippet'])
     match['snippet_text'] = textSnippet
-    if re.match(".*\W(author|writer|screenwriter)\W.*", match['snippet_text']):
+
+    if re.match(r'.*\W(author|writer|screenwriter)\W.*', match['snippet_text']):
         authorsList.append(match)
-print(authorsList)
 
 
+# searchTextList = re.split(r'\W+', args['text'])
+# searchRegexp = []
+# for r in searchTextList:
+#     if len(r)>1:
+#         searchRegexp.append("(%s)[a-z]{0,2}" % ("|".join(searchTextList)))
 
+searchTextList = re.split(r'\s+', args['text'])
+searchTextListExpanded = []
+for t in searchTextList:
+    if re.match(".+\.$", t):
+        t = '%s[\w]+' % (t[:-1])
+    searchTextListExpanded.append(t)
 
-# if DATA['query']['search'][0]['title'] == response:
-#     print("Your search page '" + response + "' exists on English Wikipedia")
+searchRegexp = []
+for r in searchTextListExpanded:
+    searchRegexp.append("(%s)[a-z]{0,2}" % ("|".join(searchTextListExpanded)))
+
+possibleNames = []
+for match in authorsList:
+    #regexp = "(%s)" % ('[^,;\(\)\[\]\/]+'.join(searchRegexp))
+    regexp = "(%s)" % ('\W+'.join(searchRegexp))
+    names = re.findall(regexp, match['snippet_text'], re.IGNORECASE)
+    for n in names:
+        possibleNames.append(n[0])
+
+if len(possibleNames):
+    print("Name: %s" % possibleNames[0])
+    exit(0)
+else:
+    print("Text not found")
+    exit(1)
