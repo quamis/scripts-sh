e559a307f1d14047f82027015eba342068e2a585
Lucian Sirbu <lucian.sirbu@dcsplus.net>
2021-11-09 09:07:51 +0200

    cc

---
+++ audio/audio.playlist.pls2m3u.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# THIS ALLOWS INJECTING VARS into the local namespace
+# might not be very secure, be careful how you declare & check variables
+for ARGUMENT in "$@"; do
+    KEY=$(echo $ARGUMENT | cut -f1 -d=)
+    VALUE=$(echo $ARGUMENT | cut -f2- -d=)
+	declare $KEY="$VALUE"
+done
+
+: ${FILE:=""};
+: ${OUTPUT:=""};
+
+: ${FILE:=""};
+
+if [[ "$FILE" == "" ]]; then
+   echo "Please specify either FILE=filename.m3u";
+   exit;
+fi
+
+EXT=${FILE##*.};
+: ${OUTPUT:="${FILE%.*}.m3u"};
+
+cat "$FILE" | egrep "^File[0-9]+=" | sed -r "s/^File[0-9]+=//" > "$OUTPUT";
+mv "$FILE" "$FILE.old";
