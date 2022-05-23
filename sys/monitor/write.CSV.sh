a418238ba0419dd3f5710637e47b8e2c7dd6035c
Lucian Sirbu <lucian.sirbu@dcsplus.net>
2022-05-23 09:08:53 +0300

    cc

--- sys/monitor/write.CSV.sh
+++ sys/monitor/write.CSV.sh
@@ -7,17 +7,17 @@ DIR=`dirname $0`;
 #   */5 * * * * cd "/var/www/html/dev/monitor/" && /home/pi/dev/scripts-sh/sys/monitor/write.CSV.sh
 
 
-HEADERS=( )
-COLUMNS=( )
+HEADERS=( );
+COLUMNS=( );
 
-COLUMNS+=(`date -Iseconds`);                    HEADERS+=("Date")
-COLUMNS+=(`$DIR/loadAvg.sh`);                   HEADERS+=("LoadAvg")
-COLUMNS+=(`$DIR/apache.connections.sh`);        HEADERS+=("ApacheConnections")
-COLUMNS+=(`$DIR/smb.connections.sh`);           HEADERS+=("SMBConnections")
-COLUMNS+=(`$DIR/cpu.temp.sh`);                  HEADERS+=("CPUTemp")
-COLUMNS+=(`$DIR/disk.internal.temp.sh sda`);    HEADERS+=("sdaTemp")
-COLUMNS+=(`$DIR/disk.usb.temp.sh sdb`);         HEADERS+=("sdbTemp")
-COLUMNS+=(`$DIR/disk.usb.temp.sh sdc`);         HEADERS+=("sdcTemp")
+COLUMNS+=(`date -Iseconds`);                    HEADERS+=("Date");
+COLUMNS+=(`$DIR/loadAvg.sh`);                   HEADERS+=("LoadAvg");
+COLUMNS+=(`$DIR/apache.connections.sh`);        HEADERS+=("ApacheConnections");
+COLUMNS+=(`$DIR/smb.connections.sh`);           HEADERS+=("SMBConnections");
+COLUMNS+=(`$DIR/cpu.temp.sh`);                  HEADERS+=("CPUTemp");
+COLUMNS+=(`$DIR/disk.internal.temp.sh sda`);    HEADERS+=("sdaTemp");
+COLUMNS+=(`$DIR/disk.usb.temp.sh sdb`);         HEADERS+=("sdbTemp");
+COLUMNS+=(`$DIR/disk.usb.temp.sh sdc`);         HEADERS+=("sdcTemp");
 
 OUT="./monitor.csv";
 if [ ! -f $OUT ]; then
