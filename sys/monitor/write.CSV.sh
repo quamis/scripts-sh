722c64ae07a575c9b614e4cc61bae08df700b2f5
Lucian Sirbu <quamis@gmail.com>
2022-05-23 08:45:40 +0300

    cc

--- sys/monitor/write.CSV.sh
+++ sys/monitor/write.CSV.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
+DIR=`dirname $0`;
+
+
 COLUMNS=( )
 
 COLUMNS+=(`date -Iseconds`);
-COLUMNS+=(`./loadAvg.sh`);
-COLUMNS+=(`./apache.connections.sh`);
-COLUMNS+=(`./smb.connections.sh`);
-COLUMNS+=(`./cpu.temp.sh`);
-COLUMNS+=(`./disk.temp.sh`);
+COLUMNS+=(`$DIR/loadAvg.sh`);
+COLUMNS+=(`$DIR/apache.connections.sh`);
+COLUMNS+=(`$DIR/smb.connections.sh`);
+COLUMNS+=(`$DIR/cpu.temp.sh`);
+COLUMNS+=(`$DIR/disk.temp.sh`);
 
 OUT="./monitor.csv";
 echo ${COLUMNS[@]} | tr " " "," >> $OUT;
