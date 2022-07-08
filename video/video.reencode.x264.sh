824e9ec0ed5c8bcbdfbcf7bc4ddef0442d3ed9d3
Lucian Sirbu <lucian.sirbu@dcsplus.net>
2022-07-08 09:32:22 +0300

    cc

--- video/video.reencode.x264.sh
+++ video/video.reencode.x264.sh
@@ -4,7 +4,7 @@
 # 	video.autoCrop.sh FILE='xyz.mkv'
 
 # re-encode all AV1 files in a folder with this:
-#     find . -type f -exec bash -c "video.is.AV1.sh FILE='{}' && video.reencode.x264.sh FILE='{}';" \;
+#     find . -type f -exec bash -c "video.is.AV1.sh FILE='{}' && video.reencode.x264.sh CLEANUP=yes FILE='{}';" \;
 
 
 # THIS ALLOWS INJECTING VARS into the local namespace
