843ac262f19d2664f9be41dea19d8318bcf4c2d2
Lucian Sirbu <quamis@gmail.com>
2024-09-03 20:57:37 +0300

    cc

--- sys/disk/disk.fill-random-data.sh
+++ sys/disk/disk.fill-random-data.sh
@@ -2,16 +2,20 @@
 
 OF="randomData";
 
-TOF="template-randomData.txt";
+TOF="/tmp/template-randomData.txt";
 DST="$1";
 # i=0; while [ $i -le 20000 ]; do openssl rand -out $OF.$i.txt -base64 $(( 4**30 )); ((i++)); done
 
-openssl rand -out $TOF -base64 $(( 4**30 ));
+openssl rand -out $TOF -base64 $(( 2**30 ));
 
 
 i=0;
 while [ $i -le 2000000 ]; do
-    cp "$TOF" "$DST/$OF.$i.txt" || { echo 'cp failed' ; exit 1; };
+    # cp "$TOF" "$DST/$OF.$i.txt" || { echo 'cp failed' ; exit 1; };
+    # pv "$TOF" > "$DST/$OF.$i.txt" || { echo 'cp failed' ; exit 1; };
+    # rsync --progress "$TOF" "$DST/$OF.$i.txt" || { echo 'cp failed' ; exit 1; };
+    rsync --info=progress2 "$TOF" "$DST/$OF.$i.txt" || { echo 'cp failed' ; exit 1; };
+
     ((i++));
 done
 
